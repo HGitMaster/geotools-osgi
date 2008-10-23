4979bf01b711b2d3899c4a2bbe404d19ec0cce4b
jdeolive <devnull@localhost>
2008-10-24 00:28:43 +0200

    [svn r31708] GEOT-1700, removed spatialdbbox dependency

--- modules/unsupported/jdbc-ng/jdbc-h2/src/test/resources/org/geotools/data/h2/h2.sql
+++ modules/unsupported/jdbc-ng/jdbc-h2/src/test/resources/org/geotools/data/h2/h2.sql
@@ -5,12 +5,12 @@
 -- 
 --       contact: dblasby@openplans.org 
 
-CREATE ALIAS AsWKT for "org.openplans.spatialdbbox.JTS.AsWKT";
-CREATE ALIAS AsText for "org.openplans.spatialdbbox.JTS.AsWKT";
-CREATE ALIAS EnvelopeAsText for "org.openplans.spatialdbbox.JTS.EnvelopeAsText";
-CREATE ALIAS GeomFromText for "org.openplans.spatialdbbox.JTS.GeomFromText";
-CREATE ALIAS Envelope for "org.openplans.spatialdbbox.JTS.Envelope";
-CREATE ALIAS GetSRID FOR "org.openplans.spatialdbbox.JTS.GetSRID";
-CREATE ALIAS GeometryType for "org.openplans.spatialdbbox.JTS.GeometryType";
+CREATE ALIAS AsWKT for "org.geotools.data.h2.JTS.AsWKT";
+CREATE ALIAS AsText for "org.geotools.data.h2.JTS.AsWKT";
+CREATE ALIAS EnvelopeAsText for "org.geotools.data.h2.JTS.EnvelopeAsText";
+CREATE ALIAS GeomFromText for "org.geotools.data.h2.JTS.GeomFromText";
+CREATE ALIAS Envelope for "org.geotools.data.h2.JTS.Envelope";
+CREATE ALIAS GetSRID FOR "org.geotools.data.h2.JTS.GetSRID";
+CREATE ALIAS GeometryType for "org.geotools.data.h2.JTS.GeometryType";
 
 COMMIT;
