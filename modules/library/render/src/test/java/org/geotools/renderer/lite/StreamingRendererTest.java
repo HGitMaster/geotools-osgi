036b8c949638f8a85e3dd8d5eb2c3c2592dc4405
aaime <devnull@localhost>
2008-11-04 17:19:04 +0100

    [svn r31767] GEOT-2049 Can trigger infinite loop in StreamingRenderer.processStylers

--- modules/library/render/src/test/java/org/geotools/renderer/lite/StreamingRendererTest.java
+++ modules/library/render/src/test/java/org/geotools/renderer/lite/StreamingRendererTest.java
@@ -16,13 +16,21 @@
  */
 package org.geotools.renderer.lite;
 
+import static org.easymock.EasyMock.*;
+
 import java.awt.Graphics2D;
 import java.awt.Rectangle;
 import java.awt.image.BufferedImage;
+import java.util.HashSet;
+import java.util.Iterator;
 
+import junit.framework.TestCase;
+
+import org.geotools.data.FeatureSource;
+import org.geotools.data.Query;
 import org.geotools.feature.FeatureCollection;
 import org.geotools.feature.FeatureCollections;
-import org.geotools.feature.IllegalAttributeException;
+import org.geotools.feature.FeatureIterator;
 import org.geotools.feature.simple.SimpleFeatureBuilder;
 import org.geotools.feature.simple.SimpleFeatureTypeBuilder;
 import org.geotools.geometry.jts.ReferencedEnvelope;
@@ -42,8 +50,6 @@ import com.vividsolutions.jts.geom.Envelope;
 import com.vividsolutions.jts.geom.GeometryFactory;
 import com.vividsolutions.jts.geom.LineString;
 
-import junit.framework.TestCase;
-
 /**
  * Test the inner workings of StreamingRenderer.
  * <p>
@@ -52,7 +58,7 @@ import junit.framework.TestCase;
  */
 public class StreamingRendererTest extends TestCase {
 
-    private SimpleFeatureType pointFeautureType;
+    private SimpleFeatureType testFeatureType;
     private GeometryFactory gf = new GeometryFactory();
     protected int errors;
     protected int features;
@@ -62,7 +68,7 @@ public class StreamingRendererTest extends TestCase {
         SimpleFeatureTypeBuilder builder = new SimpleFeatureTypeBuilder();
         builder.setName("Lines");
         builder.add("geom", LineString.class, DefaultGeographicCRS.WGS84);
-        pointFeautureType = builder.buildFeatureType();
+        testFeatureType = builder.buildFeatureType();
     }
 
     public FeatureCollection<SimpleFeatureType, SimpleFeature> createLineCollection() throws Exception {
@@ -75,7 +81,7 @@ public class StreamingRendererTest extends TestCase {
 
     private SimpleFeature createLine(double x1, double y1, double x2, double y2) {
         Coordinate[] coords = new Coordinate[] { new Coordinate(x1, y1), new Coordinate(x2, y2) };
-        return SimpleFeatureBuilder.build(pointFeautureType, new Object[] { gf.createLineString(coords) }, null);
+        return SimpleFeatureBuilder.build(testFeatureType, new Object[] { gf.createLineString(coords) }, null);
     }
 
     private Style createLineStyle() {
@@ -119,4 +125,62 @@ public class StreamingRendererTest extends TestCase {
         assertTrue( features > 0 );
     }
 
+    public void testInfiniteLoopAvoidance() throws Exception {
+        final Exception sentinel = new RuntimeException("This is the one that should be thrown in hasNext()");
+        
+        // setup the mock necessary to have the renderer hit into the exception in hasNext()
+        FeatureIterator it2 = createNiceMock(FeatureIterator.class);
+        expect(it2.hasNext()).andThrow(sentinel).anyTimes();
+        replay(it2);
+        
+        FeatureCollection fc = createNiceMock(FeatureCollection.class);
+        expect(fc.features()).andReturn(it2);
+        expect(fc.size()).andReturn(200);
+        expect(fc.getSchema()).andReturn(testFeatureType).anyTimes();
+        replay(fc);
+        
+        FeatureSource fs = createNiceMock(FeatureSource.class);
+        expect(fs.getFeatures((Query) anyObject())).andReturn(fc);
+        expect(fs.getSchema()).andReturn(testFeatureType).anyTimes();
+        expect(fs.getSupportedHints()).andReturn(new HashSet());
+        replay(fs);
+        
+        // build map context
+        MapContext mapContext = new DefaultMapContext(DefaultGeographicCRS.WGS84);
+        mapContext.addLayer(fs, createLineStyle());
+        
+        // setup the renderer and listen for errors
+        final StreamingRenderer sr = new StreamingRenderer();
+        sr.setContext(mapContext);
+        sr.addRenderListener(new RenderListener() {
+            public void featureRenderer(SimpleFeature feature) {
+                features++;
+            }
+            public void errorOccurred(Exception e) {
+                errors++;
+                
+                if(errors > 2) {
+                    // we dont' want to block the loop in case of regression on this bug
+                    sr.stopRendering();
+                }
+
+                // but we want to make sure we're getting
+                Throwable t = e;
+                while(t != sentinel && t.getCause() != null)
+                    t = t.getCause();
+                assertSame(sentinel, t);
+            }
+        });
+        errors = 0;
+        features = 0;
+        BufferedImage image = new BufferedImage(200, 200,BufferedImage.TYPE_4BYTE_ABGR);
+        ReferencedEnvelope reWgs = new ReferencedEnvelope(new Envelope(-180,
+                -170, 20, 40), DefaultGeographicCRS.WGS84);
+        sr.paint((Graphics2D) image.getGraphics(), new Rectangle(200, 200),reWgs);
+        
+        // we should get two errors since there are two features that cannot be
+        // projected but the renderer itself should not throw exceptions
+        assertEquals(0, features);
+        assertEquals(1, errors);
+    }
 }
