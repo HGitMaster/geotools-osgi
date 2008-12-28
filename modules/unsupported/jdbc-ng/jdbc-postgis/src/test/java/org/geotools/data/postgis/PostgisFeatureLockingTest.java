8ceb601eebca20a2e4f64e7def6d29d9da6ae6c5
aaime <devnull@localhost>
2008-12-28 14:45:32 +0100

    [svn r32096] Feature locking test

---
+++ modules/unsupported/jdbc-ng/jdbc-postgis/src/test/java/org/geotools/data/postgis/PostgisFeatureLockingTest.java
@@ -0,0 +1,13 @@
+package org.geotools.data.postgis;
+
+import org.geotools.jdbc.JDBCFeatureLockingTest;
+import org.geotools.jdbc.JDBCTestSetup;
+
+public class PostgisFeatureLockingTest extends JDBCFeatureLockingTest {
+
+    @Override
+    protected JDBCTestSetup createTestSetup() {
+        return new PostGISTestSetup();
+    }
+
+}
