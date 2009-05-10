99e770931e2e6212a67e234ff5103491c9218efa
jive <devnull@localhost>
2009-05-10 04:06:04 +0200

    [svn r32969] Add feature event notification to ContentState and ContentEntry

--- modules/library/data/src/main/java/org/geotools/data/store/ContentState.java
+++ modules/library/data/src/main/java/org/geotools/data/store/ContentState.java
@@ -17,12 +17,24 @@
 package org.geotools.data.store;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.logging.Level;
 
+import org.geotools.data.BatchFeatureEvent;
+import org.geotools.data.FeatureEvent;
 import org.geotools.data.FeatureListener;
+import org.geotools.data.FeatureSource;
 import org.geotools.data.Transaction;
+import org.geotools.data.FeatureEvent.Type;
+import org.geotools.factory.CommonFactoryFinder;
 import org.geotools.geometry.jts.ReferencedEnvelope;
+import org.opengis.feature.Feature;
 import org.opengis.feature.simple.SimpleFeatureType;
+import org.opengis.filter.Filter;
+import org.opengis.filter.FilterFactory2;
+import org.opengis.filter.identity.FeatureId;
 
 /**
  * The state of an entry in a datastore, maintained on a per-transaction basis.
@@ -76,21 +88,30 @@ public class ContentState {
      */
     protected Transaction tx;
 	/**
-	 * cached feature type
-	 */
-	protected SimpleFeatureType featureType;
-	/**
-	 * cached number of features
-	 */
-	protected int count = -1;
-	/**
-	 * cached bounds of features
-	 */
-	protected ReferencedEnvelope bounds;
+     * cached feature type
+     */
+    protected SimpleFeatureType featureType;
+
+    /**
+     * cached number of features
+     */
+    protected int count = -1;
+
+    /**
+     * cached bounds of features
+     */
+    protected ReferencedEnvelope bounds;
+
     /**
      * entry maintaining the state
      */
     protected ContentEntry entry;
+    
+    /**
+     * Even used for batch notification; used to collect the bounds and feature ids generated
+     * over the course of a transaction.
+     */
+    protected BatchFeatureEvent batchFeatureEvent;
     
     /**
      * observers
@@ -140,7 +161,7 @@ public class ContentState {
     }
     
     /**
-     * Sets the transaction associatred with the state.
+     * Sets the transaction associated with the state.
      */
     public void setTransaction(Transaction tx) {
         this.tx = tx;
@@ -149,11 +170,11 @@ public class ContentState {
     /**
      * The cached feature type.
      */
-	public final SimpleFeatureType getFeatureType(){
+    public final SimpleFeatureType getFeatureType(){
     	return featureType;
     }
     
-	/**
+    /**
      * Sets the cached feature type.
      */
     public final void setFeatureType( SimpleFeatureType featureType ){
@@ -208,6 +229,133 @@ public class ContentState {
     }
 
     /**
+     * Used to quickly test if any listeners are available.
+     * @return
+     */
+    public final boolean hasListener(){
+        if( !listeners.isEmpty() ){
+            return true;
+        }
+        if( this.tx == Transaction.AUTO_COMMIT && this.entry.state.size() > 1){
+            // We are the auto commit state; and there is at least one other thread to notify
+            return true;
+        }
+        return false;
+    }
+    public void fireFeatureUpdated(FeatureSource<?,?> source, Feature feature,
+            ReferencedEnvelope before) {
+        if( listeners.isEmpty() && tx != Transaction.AUTO_COMMIT) return;
+        
+        FilterFactory2 ff = CommonFactoryFinder.getFilterFactory2(null);
+        Set<FeatureId> fids = new HashSet<FeatureId>();
+        fids.add( feature.getIdentifier() );
+        Filter filter = ff.id( fids );
+        ReferencedEnvelope bounds = new ReferencedEnvelope( feature.getBounds() );
+        bounds.expandToInclude( before );
+        
+        FeatureEvent event = new FeatureEvent(source, Type.CHANGED, bounds, filter );        
+        fireFeatureEvent( event );
+    }
+
+    /**
+     * Used to issue a Type.ADDED FeatureEvent indicating a new feature being created
+     * @param source
+     * @param feature
+     */
+    public final void fireFeatureAdded( FeatureSource<?,?> source, Feature feature ){
+        if( listeners.isEmpty() && tx != Transaction.AUTO_COMMIT) return;
+        
+        FilterFactory2 ff = CommonFactoryFinder.getFilterFactory2(null);
+        Set<FeatureId> fids = new HashSet<FeatureId>();
+        fids.add( feature.getIdentifier() );
+        Filter filter = ff.id( fids );
+        ReferencedEnvelope bounds = new ReferencedEnvelope( feature.getBounds() );
+        
+        FeatureEvent event = new FeatureEvent(source, Type.ADDED, bounds, filter );
+        
+        fireFeatureEvent( event );
+    }
+
+    public void fireFeatureRemoved(FeatureSource<?,?> source, Feature feature) {
+        if( listeners.isEmpty() && tx != Transaction.AUTO_COMMIT) return;
+        
+        FilterFactory2 ff = CommonFactoryFinder.getFilterFactory2(null);
+        Set<FeatureId> fids = new HashSet<FeatureId>();
+        fids.add( feature.getIdentifier() );
+        Filter filter = ff.id( fids );
+        ReferencedEnvelope bounds = new ReferencedEnvelope( feature.getBounds() );
+        
+        FeatureEvent event = new FeatureEvent(source, Type.REMOVED, bounds, filter );
+        
+        fireFeatureEvent( event );        
+    }
+
+    /**
+     * Used to issue a single FeatureEvent.
+     * <p>
+     * If this content state is used for Transaction.AUTO_COMMIT the notification
+     * will be passed to all interested parties.
+     * <p>
+     * If not this event will be recored as part of a BatchFeatureEvent that will
+     * to be issued using issueBatchFeatureEvent() 
+     * @param event
+     */
+    public final void fireFeatureEvent( FeatureEvent event){
+        if( this.tx == Transaction.AUTO_COMMIT ){
+            this.entry.notifiyFeatureEvent(this, event );
+        }
+        else {
+            // we are not in auto-commit mode so we need to batch       
+            // up the changes for when the commit goes out
+            if( batchFeatureEvent == null ){
+                batchFeatureEvent = new BatchFeatureEvent( event.getFeatureSource() );
+            }
+            batchFeatureEvent.add( event );
+        }
+        if( listeners.isEmpty() ){
+            return;           
+        }
+        for (FeatureListener listener : listeners) {
+            try {
+                listener.changed( event );
+            }
+            catch (Throwable t ){
+                this.entry.dataStore.LOGGER.log( Level.WARNING, "Problem issuing batch feature event "+event, t );
+            }
+        }
+    }
+    /**
+     * Notifies all waiting listeners that a commit has been issued;
+     * this notification is also sent to our  
+     */
+    public final void fireBatchFeatureEvent(boolean isCommit){
+       if( batchFeatureEvent == null ){
+           return;
+       }
+       if( listeners.isEmpty() ){
+           return;           
+       }
+       if( isCommit ){
+           batchFeatureEvent.setType( Type.COMMIT );
+       }
+       else {
+           batchFeatureEvent.setType( Type.ROLLBACK );
+       }
+       for (FeatureListener listener : listeners) {
+           try {
+               listener.changed( batchFeatureEvent );
+           }
+           catch (Throwable t ){
+               this.entry.dataStore.LOGGER.log( Level.WARNING, "Problem issuing batch feature event "+batchFeatureEvent, t );
+           }
+       }
+       // Let others know a modifications was made
+       this.entry.notifiyFeatureEvent(this, batchFeatureEvent );
+       
+       batchFeatureEvent = null;
+    }
+    
+    /**
      * Clears cached state.
      * <p>
      * This method does not affect any non-cached state. This method may be 
@@ -219,7 +367,7 @@ public class ContentState {
         count = -1;
         bounds = null;
     }
-    
+        
     /**
      * Clears all state.
      * <p>
@@ -248,4 +396,5 @@ public class ContentState {
     public ContentState copy() {
         return new ContentState( this );
     }
+
 }
